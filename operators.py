bc37efc76440c7261bd534ae220be03055ea65ec
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2023-01-15 10:24:52 +0545

    basic concept of operators used in python

---
+++ operators.py
@@ -0,0 +1,32 @@
+a=6
+b=4
+x=2
+list=[1,2,3,4,5,6,7,8]
+#arithmetic operators
+c=a+b
+d=a-b
+e=a**3
+f=a%b
+g=a/b
+print("c=",c,"d=",d,"e=",e,"f=",f,"g=",g)
+
+#relational operators
+if(a>b): print("wanda vision")
+
+#logical operators
+if(a!=b): print (c*d);
+if(b<a or x>b): print("hello world")
+
+#identity operator
+if(type(a)is float): print("correct") 
+else: print("incorrect")
+if(type(g) is not int): print("correct")
+else: print("incorrect")
+
+#membership operators
+if(type(c) in list): print("c is in list")
+else: print("c is not in list")
+if(type(d) not in list): print("d is in list")
+else: print("d is not in list")
+
+
