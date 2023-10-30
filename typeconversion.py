311cb0ef8d00c2d956fa329f76d475dc2df4c5cc
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2023-10-30 19:14:59 +0545

    implicit and explicit type conversion

---
+++ typeconversion.py
@@ -0,0 +1,21 @@
+#implicit conversion
+#int_valuee = 50
+#float_value = 1.50
+
+#new1= int_valuee+float_value
+#print(new1) 
+#print(type(new1)) //float
+
+
+#explicit conversion
+int_value=55
+str_value= '10'
+str_value=int(str_value)
+
+new2= int_value+str_value
+print(new2)
+print(type(new2))
+
+
+
+
