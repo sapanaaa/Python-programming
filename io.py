77d55ac174b2804882f2fcbf6b8350d76cff8e93
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2023-10-31 19:20:19 +0545

    input output in python

---
+++ io.py
@@ -0,0 +1,9 @@
+#print() statement is used to display output
+
+# print("i am ironman")
+# print("i love you 3000")
+
+#input()statement is used to give input
+
+num =input("enter a number:") #asks user to give input 
+print("the number you entered is:", num) #displays the value entered by user
