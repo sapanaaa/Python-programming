180433d6d54f96bdcb90e8e3b0f0b12aa19d12d5
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2023-11-05 14:15:20 +0545

    if else

---
+++ ifElse.py
@@ -0,0 +1,8 @@
+
+# to find greatest among 3 numbers
+a= int(input("a="))
+b= int(input("b="))
+c= int(input("c="))
+if( (a>b) and (a>c)): print("a=",a, "is the greatest")
+elif( (b>a) and (b>c)): print("c=",b, "is the greatest")
+else: print("c=",c, "is the greatest ")
