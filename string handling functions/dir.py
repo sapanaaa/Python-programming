32a9eeba51baffff72f75af8a24a9af84fe43486
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2024-07-02 17:50:29 +0545

    returns all properties of an object

--- string handling functions/dir.py
+++ string handling functions/dir.py
@@ -1,4 +1,4 @@
-#dir functions returns all properties and methods and even inbuilt properties of a certain object
+#dir function returns all properties and methods and even inbuilt properties of a certain object
 class person:
     name= "sapana"
     age= 20
