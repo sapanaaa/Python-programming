b3edb690473453425e0815aec4eacfe0f3c5f4c3
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2024-07-15 20:42:17 +0545

    perform floor division

---
+++ numbers and strings/floorDivision.py
@@ -0,0 +1,16 @@
+#code 7 of applying various logics to perform floor division
+
+#1
+'''
+import math
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+div1=math.floor(x/y)
+print(div1)
+'''
+
+#2
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+div=x//y #// is returns floor value of a floating number
+print(div)
