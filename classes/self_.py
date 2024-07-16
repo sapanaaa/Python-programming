261c810977539ad4a7d18c2f4e5d7df4f216f343
Sapana Pandey <114503539+sapanaaa@users.noreply.github.com>
2024-07-16 20:39:44 +0545

    self or selff doesn't matter

---
+++ classes/self_.py
@@ -0,0 +1,12 @@
+class person:
+    name="sapana"
+    age=20
+    address="bhasi"
+    def bio(selff):
+        print(f"{selff.name} is {selff.age} years old")
+a= person()
+a.bio()
+b=person()
+b.name="misty"
+b.age=3
+b.bio()
